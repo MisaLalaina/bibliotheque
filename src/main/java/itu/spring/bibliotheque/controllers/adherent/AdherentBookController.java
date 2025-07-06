8f036516589ca353a42eea4949cfa22deadcb2fc
MisaLalaina <rakotoarivonylalainamisa@gmail.com>
2025-07-06 20:14:17 +0300

    fix import

--- src/main/java/itu/spring/bibliotheque/controllers/adherent/AdherentBookController.java
+++ src/main/java/itu/spring/bibliotheque/controllers/adherent/AdherentBookController.java
@@ -7,10 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import itu.spring.bibliotheque.models.Adherent;
-import itu.spring.bibliotheque.models.Book;
 import itu.spring.bibliotheque.models.Utilisateur;
 import itu.spring.bibliotheque.models.dto.BookLoan;
 import itu.spring.bibliotheque.services.AdherentService;
